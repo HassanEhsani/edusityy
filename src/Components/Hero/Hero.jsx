644a23ffb2b331bbb1d90ec46c81fd0eb9c1b22d
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 21:13:48 +0300

    i puted hero-text on the center of the page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,8 +4,12 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculm is designed to empower students with the knowlege, skills. and experiences needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore more</button>
+      </div>
     </div>
   )
 }
