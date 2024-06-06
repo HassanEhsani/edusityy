891e69661b843702539e1ce2c533b8a890bf2ff9
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 21:29:18 +0300

    i added the program_1 img

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Programs.css'
+import program_1 from '../../assets/program-1.png'
 
 const Programs = () => {
   return (
     <div className='programs'>
       <div className="program">
-        
+        <img src={program_1} alt="" />
       </div>
     </div>
   )
