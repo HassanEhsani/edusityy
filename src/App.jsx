b61ef57acf6db22b36b0df7a0fe3a507f0c862e2
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 20:14:00 +0300

    i created navbar folder and file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   )
 }
