0fbf63063c4ad284516f8e7fd4182c1a807f5e58
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 21:40:19 +0300

    i managed the photo about the programs in the css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Programs/>
     </div>
   )
 }
