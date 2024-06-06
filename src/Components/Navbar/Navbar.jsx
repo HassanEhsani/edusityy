7b86a3c9b581b09b48fb97525c7ff12d1eb6c25d
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 23:53:58 +0300

    i wrote this code ({`container ${sticky ? 'dark-nav' : ''}`}) about dark-nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
-  return (
-   <nav className='container dark-nav'>
-    <img src={logo} alt="" className='logo'/>
-    <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
-    </ul>
-   </nav>
-  )
+    const [sticky, setSticky] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setSticky(true) : setSticky(false);
+        })
+    }, []);
+    return (
+        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+            <img src={logo} alt="" className='logo' />
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About Us</li>
+                <li>Campus</li>
+                <li>Testimonials</li>
+                <li><button className='btn'>Contact Us</button></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
