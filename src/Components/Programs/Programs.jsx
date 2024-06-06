421676333f6a6776c58839f2d494ac2247670575
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 21:27:36 +0300

    i created programs folder and file jsx and css

---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Programs.css'
+
+const Programs = () => {
+  return (
+    <div className='programs'>
+      <div className="program">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Programs
