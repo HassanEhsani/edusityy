b1cf6cef625f3c1b9a0b2451bf4e15bd1415ce82
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-09 08:35:19 +0300

    added silde in the page

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -9,13 +9,64 @@ import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
-  return (
-    <div className='testimonials'>
-      <img src={next_icon} alt="" className='next-btn'/>
-      <img src={back_icon} alt="" className='back-btn'/>
-      <div className="slider"></div>
-    </div>
-  )
+    return (
+        <div className='testimonials'>
+            <img src={next_icon} alt="" className='next-btn' />
+            <img src={back_icon} alt="" className='back-btn' />
+            <div className="slider">
+                <ul>
+                    <li>
+                        <div className="silde">
+                            <div className="user-info">
+                                <img src={user_1} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportinve community, state-of-the-art favilities, and commitment to academic excellence have truly exceeded my expeectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="silde">
+                            <div className="user-info">
+                                <img src={user_2} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportinve community, state-of-the-art favilities, and commitment to academic excellence have truly exceeded my expeectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="silde">
+                            <div className="user-info">
+                                <img src={user_3} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportinve community, state-of-the-art favilities, and commitment to academic excellence have truly exceeded my expeectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="silde">
+                            <div className="user-info">
+                                <img src={user_4} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportinve community, state-of-the-art favilities, and commitment to academic excellence have truly exceeded my expeectations.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Testimonials
