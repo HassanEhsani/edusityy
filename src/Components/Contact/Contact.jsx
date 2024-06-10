dbe87b2d4bb6e59cb750b241e9b029d86a5807d0
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-10 23:51:18 +0300

    added 3 icon for contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import './Contact.css'
 import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
 
 const Contact = () => {
     return (
@@ -10,9 +13,9 @@ const Contact = () => {
                 <h3>Send us a message <img src={msg_icon} alt="" /></h3>
                 <p>Feel free to reach out through contact from or find our contact information below. your feedback, questions, and suggextions are important to us as we strive to provide exceptional service to our university community.</p>
                 <ul>
-                    <li>Hassan.ehsani.am@gmail.com</li>
-                    <li>+7 977-975-2029</li>
-                    <li>Russain Sant Petersburg</li>
+                    <li><img src={mail_icon} alt="" />Hassan.ehsani.am@gmail.com</li>
+                    <li><img src={phone_icon} alt="" />+7 977-975-2029</li>
+                    <li><img src={location_icon} alt="" />Russain Sant Petersburg</li>
                 </ul>
             </div>
             <div className="contact-col"></div>
