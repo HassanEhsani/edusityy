0aa58d12b99ed2e804b896854f0071beed68b6ea
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-13 14:00:26 +0300

    adjust the offset about the navbar in the react scroll

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
             <img src={logo} alt="" className='logo' />
             <ul>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-                <li><Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
-                <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link></li>
-                <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
-                <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
-                <li><button to='testimonials' offset={0} className='btn'>Contact Us</button></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><button to='contact'  offset={0} className='btn'>Contact Us</button></li>
             </ul>
         </nav>
     )
