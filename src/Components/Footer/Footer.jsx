ba33d927e4f28adf0b11cf74257f721b99c8aab3
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-13 12:18:34 +0300

    added footer in the App.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,12 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
+    <p>@ 2024 Edusity. All rights reserved.</p>
+    <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+        <li></li>
+    </ul>
       
     </div>
   )
