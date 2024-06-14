d65f9e76eb995b47326436a5aed8d7d5d2f33673
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-14 03:32:28 +0300

    wrote the code about when the user close the video click anywhere and closed (const player = useRef(null);)

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useRef } from 'react'
 import './VideoPlayer.css'
 import video from '../../assets/video.mp4'
 
 const VideoPlayer = ({playState, setPlayState}) => {
+    const player = useRef(null);
   return (
-    <div className={`video-player ${playState? '':'hide'}`} >
+    <div className={`video-player ${playState? '':'hide'}`}ref={player} >
       <video src={video} autoPlay muted controls></video>
     </div>
   )
